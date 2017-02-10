fd7dacd35a7b202bf7ae363d4e3edc4c2cbe8987
Wenchen Li <wenchen.li.cs@gmail.com>
2017-02-09 16:45:47 -0800

    install torch

---
+++ shell/install_torch.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/torch/distro.git ~/frameworks/torch --recursive
+cd ~/frameworks/torch; bash install-deps;
+./install.sh
+# On Linux with bash
+source ~/.bashrc
+# On Linux with zsh
+source ~/.zshrc
+# On OSX or in Linux with none of the above.
+source ~/.profile
