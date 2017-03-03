5c904665c1483fbbeb75d6e2a593562aaf0ca606
Wenchen Li <wenchen.li.cs@gmail.com>
2017-03-03 17:08:38 -0600

    add install pinyin

---
+++ shell/install_pinyin.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+pinyin=$1
+if [ "$pinyin" = "sogou" ];
+    then echo "please go to http://pinyin.sogou.com/linux/?r=pinyin and
+    download your version then do sudo dpkg -i ~/Downloads/sogoupinyin*.deb; sudo apt -f install"
+else
+    echo "installing googlepinyin"
+    sudo apt install fcitx fcitx-googlepinyin
+fi
+
